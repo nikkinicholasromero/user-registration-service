650f0e1bf7278fe485cc8ffed03ec98bc66354d3
Nikki Nicholas Romero <nikkinicholas.romero@gmail.com>
2020-07-13 06:23:46 -0400

    Initial commit

---
+++ src/main/java/com/demo/ApplicationConfig.java
@@ -0,0 +1,33 @@
+package com.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class ApplicationConfig {
+    @Value("${demo.database.driver}")
+    private String driver;
+
+    @Value("${demo.database.url}")
+    private String url;
+
+    @Value("${demo.database.username}")
+    private String username;
+
+    @Value("${demo.database.password}")
+    private String password;
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(driver);
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        return dataSource;
+    }
+}
