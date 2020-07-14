c953a258e01644c420b513fd3cbf168e5e5cb81d
Nikki Nicholas Romero <nikkinicholas.romero@gmail.com>
2020-07-14 17:23:02 -0400

    Refactored ErrorHandlerAdvice

--- src/main/java/com/demo/controller/ErrorHandlerAdvice.java
+++ src/main/java/com/demo/controller/ErrorHandlerAdvice.java
@@ -1,9 +1,7 @@
 package com.demo.controller;
 
-import com.demo.model.ErrorDetails;
 import com.demo.model.ErrorResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -13,44 +11,34 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class ErrorHandlerAdvice {
     @Autowired
-    private MessageSource messageSource;
+    private ErrorResponseBuilder errorResponseBuilder;
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
-        List<FieldError> errors = ex.getBindingResult().getFieldErrors();
-        ErrorResponse errorResponse = buildErrorResponse(
-                errors.stream().map(FieldError::getDefaultMessage).collect(Collectors.toList()));
-        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+        List<String> errorCodes = ex.getBindingResult().getFieldErrors().stream()
+                .map(FieldError::getDefaultMessage)
+                .collect(Collectors.toList());
+
+        return buildResponseEntity(errorCodes);
     }
 
     @ExceptionHandler(ConstraintViolationException.class)
     public ResponseEntity<ErrorResponse> handleConstraintViolationException(ConstraintViolationException ex) {
-        Set<ConstraintViolation<?>> errors = ex.getConstraintViolations();
-        ErrorResponse errorResponse = buildErrorResponse(
-                errors.stream().map(ConstraintViolation::getMessage).collect(Collectors.toList()));
-        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+        List<String> errorCodes = ex.getConstraintViolations().stream()
+                .map(ConstraintViolation::getMessage)
+                .collect(Collectors.toList());
+
+        return buildResponseEntity(errorCodes);
     }
 
-    private ErrorResponse buildErrorResponse(List<String> errorCodes) {
-        List<ErrorDetails> errorDetails = new ArrayList<>();
-        for (String errorCode : errorCodes) {
-            ErrorDetails error = new ErrorDetails();
-            error.setCode(messageSource.getMessage(errorCode + ".code", null, Locale.US));
-            error.setMessage(messageSource.getMessage(errorCode + ".message", null, Locale.US));
-            errorDetails.add(error);
-        }
-
-        ErrorResponse errorResponse = new ErrorResponse();
-        errorResponse.setErrors(errorDetails);
-        return errorResponse;
+    private ResponseEntity<ErrorResponse> buildResponseEntity(List<String> errorsString) {
+        ErrorResponse errorResponse = errorResponseBuilder.build(errorsString);
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 }
