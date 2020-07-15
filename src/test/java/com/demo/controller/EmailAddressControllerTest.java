7b2a40b499dd249ee7ee152cc4c64444f83a510e
Nikki Nicholas Romero <nikkinicholas.romero@gmail.com>
2020-07-14 22:12:35 -0400

    Fixed failing controller tests

--- src/test/java/com/demo/controller/EmailAddressControllerTest.java
+++ src/test/java/com/demo/controller/EmailAddressControllerTest.java
@@ -5,12 +5,10 @@ import com.demo.service.EmailAddressService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.mockito.ArgumentMatchers.anyString;
@@ -20,24 +18,17 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest
-@WebAppConfiguration
+@SpringBootTest
+@AutoConfigureMockMvc
 public class EmailAddressControllerTest {
-    private MockMvc mockMvc;
-
     @Autowired
-    private WebApplicationContext context;
+    private MockMvc mockMvc;
 
     @MockBean
     private EmailAddressService emailAddressService;
 
-    @MockBean
-    private ErrorHandlerAdvice errorHandlerAdvice;
-
     @BeforeEach
     public void setup() {
-        mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
-
         when(emailAddressService.getEmailAddressStatus(anyString())).thenReturn(EmailAddressStatus.ACTIVATED);
     }
 
