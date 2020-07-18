1d833c142db125d514775a69cdc701777b0a0647
Nikki Nicholas Romero <nikkinicholas.romero@gmail.com>
2020-07-18 14:09:11 -0400

    Added unit tests for external service classes

--- src/main/java/com/demo/external/email/EmailService.java
+++ src/main/java/com/demo/external/email/EmailService.java
@@ -1,22 +1,24 @@
 package com.demo.external.email;
 
-import com.demo.external.hash.HashRequest;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class EmailService {
-    @Autowired
-    private RestTemplate restTemplate;
+    private final RestTemplate restTemplate;
 
     @Value("${email-service.send.host}")
     private String host;
 
     @Value("${email-service.send.endpoint}")
     private String endpoint;
+
+    public EmailService(RestTemplateBuilder restTemplateBuilder) {
+        restTemplate = restTemplateBuilder.build();
+    }
 
     public void send(Mail mail) {
         HttpEntity<Mail> httpEntity = new HttpEntity<>(mail);
--- src/main/java/com/demo/external/hash/HashService.java
+++ src/main/java/com/demo/external/hash/HashService.java
@@ -1,21 +1,24 @@
 package com.demo.external.hash;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class HashService {
-    @Autowired
-    private RestTemplate restTemplate;
+    private final RestTemplate restTemplate;
 
     @Value("${hash-service.hash.host}")
     private String host;
 
     @Value("${hash-service.hash.endpoint}")
     private String endpoint;
+
+    public HashService(RestTemplateBuilder builder) {
+        restTemplate = builder.build();
+    }
 
     public String hash(String clear, String salt) {
         HashRequest hashRequest = new HashRequest(clear, salt);
